4d7c69729c170b89337b486d425477278c378874
Miguel de Moura <me@migueldemoura.com>
2018-03-09 18:55:32 +0000

    Update share buttons

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -5,10 +5,4 @@
 $(function() {
     // Fluidbox
     $('.fluidbox-trigger').fluidbox();
-
-    // Share buttons
-    $('.article-share a').on('click', function () {
-        window.open($(this).attr('href'), 'Share', 'width=200,height=200,noopener');
-        return false;
-    });
 });
