7bfd6a8193546fa8b3bd9bee663fef1766adc69c
Miguel de Moura <me@migueldemoura.com>
2018-03-17 23:15:47 +0000

    Add Turbolinks

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,3 +1,7 @@
 //= require zoom.js
+//= require turbolinks/dist/turbolinks.js
 
-new Zooming();
+new Zooming({customSize: '100%'});
+document.addEventListener('turbolinks:load', function() {
+    new Zooming({customSize: '100%'});
+})
