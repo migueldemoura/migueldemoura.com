2ac76efbc9ea4fdcc30819689b23eae94f1a71f1
Miguel de Moura <me@migueldemoura.com>
2018-04-03 23:12:46 +0100

    Replace forked zooming with upstream

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,7 +1,7 @@
-//= require zoom.js
 //= require turbolinks/dist/turbolinks.js
+//= require zooming/build/zooming.js
 
-new Zooming({customSize: '100%'});
+new Zooming({customSize: '100%', scaleBase: 0.9, scaleExtra: 0, enableGrab: false});
 document.addEventListener('turbolinks:load', function() {
-    new Zooming({customSize: '100%'});
+    new Zooming({customSize: '100%', scaleBase: 0.9, scaleExtra: 0, enableGrab: false});
 })
