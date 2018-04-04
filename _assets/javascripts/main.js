7de560f38a126ed9d17263cf4a2769ac48338950
Miguel de Moura <me@migueldemoura.com>
2018-04-04 01:21:35 +0100

    Remove duplicate zooming declaration

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,7 +1,6 @@
 //= require turbolinks/dist/turbolinks.js
 //= require zooming/build/zooming.js
 
-new Zooming({customSize: '100%', scaleBase: 0.9, scaleExtra: 0, enableGrab: false});
 document.addEventListener('turbolinks:load', function() {
     new Zooming({customSize: '100%', scaleBase: 0.9, scaleExtra: 0, enableGrab: false});
 })
