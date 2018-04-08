520d6084172c30c2c5a13b8ef1150da26df04944
Miguel de Moura <me@migueldemoura.com>
2018-04-08 19:15:06 +0100

    Update zooming

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -2,5 +2,7 @@
 //= require zooming/build/zooming.js
 
 document.addEventListener('turbolinks:load', function() {
-    new Zooming({customSize: '100%', scaleBase: 0.9, scaleExtra: 0, enableGrab: false});
+    new Zooming(
+        {customSize: '100%', scaleBase: 0.9, scaleExtra: 0, enableGrab: false}
+    ).listen('img[data-action="zoom"]');
 })
