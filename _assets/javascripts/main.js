ee41371a1823a8646d4efaa1b27ba21020fff75d
Miguel de Moura <me@migueldemoura.com>
2018-09-03 20:55:15 +0100

    Fix blurry zoomed in image on Blink/Webkit

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -3,8 +3,13 @@
 
 /* global Zooming */
 
-document.addEventListener('turbolinks:load', function() {
-  new Zooming(
-    {customSize: '100%', scaleBase: 0.9, scaleExtra: 0, enableGrab: false}
-  ).listen('img[data-action="zoom"]');
+document.addEventListener('turbolinks:load', () => {
+  new Zooming({
+    customSize: '100%',
+    scaleBase: 0.9,
+    scaleExtra: 0,
+    enableGrab: false,
+    onBeforeOpen: (e) => e.style['border-radius'] = '0',
+    onBeforeClose: (e) => e.style['border-radius'] = ''
+  }).listen('img[data-action="zoom"]');
 })
